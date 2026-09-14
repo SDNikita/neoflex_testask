2b7d7957b3d36f6871ddc60311d690dc56d487a5
SDNikita <sidnevnikitait@gmail.com>
2026-09-14 16:58:32 +0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     {/* head*/}
+     <section>
+       <p> QPICK</p>
+     </section>
+
+      {/*наушниками*/}
+      <section>
+
+      </section>
+
+      {/*Беспроводные наушниками*/}
+
+      <section>
+
+      </section>
+    {/* Footer*/}
+      <section>
+
+      </section>
+    </>
+  )
+}
+
+export default App
