7b5ff0b9c5ce5f7a645b4c07cd79987afbc5e405
SDNikita <sidnevnikitait@gmail.com>
2026-09-15 16:42:02 +0300

    create database headphones

---
+++ src/data/Headphones.js
@@ -0,0 +1,27 @@
+import headphones1 from '../../assets/images/headheadphones1'
+import headphones2 from '../../assets/images/headheadphones2'
+import headphones3 from '../../assets/images/headheadphones3'
+
+const headphones = [
+    {
+        id:1,
+        img:headphones1,
+        title:'Apple BYZ S852I',
+        price:2397,
+        rate: 4.7
+    },
+    {
+        id:2,
+        img:headphones2,
+        title:'Apple EarPods',
+        price:2327,
+        rate: 4.5
+    },
+    {
+        id:3,
+        img:headphones3,
+        title:'Apple EarPods Pro',
+        price:2327,
+        rate: 4.5
+    }
+]
